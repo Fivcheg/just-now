bf343f2472109b1fdf76c777c57543e52f2d88ed
Vitalii Novgorodov <fivcheg@gmail.com>
2022-10-12 21:03:21 +0300

    Add fun agoClock

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val timeInSeconds = 1261
+    val timeInSeconds = 82000
     val finish = agoToText(timeInSeconds)
     println("Был(-а) в сети $finish")
 }
@@ -18,16 +18,27 @@ fun agoMinutes(timeInSecond: Int): String{
     }
     return resultMinutes
 }
-/*
-fun agoClock(timeInSecond: Int): String{
 
-}*/
+fun agoClock(timeInSecond: Int): String{
+    val transformToClock = timeInSecond / 3600
+    val resultClock: String
+    if (transformToClock == 1 || transformToClock%10 == 1 && transformToClock!=11) {
+        resultClock = "$transformToClock час назад"
+    }
+    else if (transformToClock in 2..4 || transformToClock%10 in 2..4 && transformToClock>20) {
+        resultClock= "$transformToClock часа назад"
+    }
+    else {
+        resultClock = "$transformToClock часов назад"
+    }
+    return resultClock
+}
 
 fun agoToText(timeInSecond: Int): String{
     val result = when (timeInSecond){
         in 0..60 -> "только что"
         in 61..60*60 -> agoMinutes(timeInSecond)
-        in 60*60+1..24*60*60 -> ""//agoClock(timeInSecond)
+        in 60*60+1..24*60*60 -> agoClock(timeInSecond)
         in 24*60*60+1..2*24*60*60 -> "вчера"
         in 2*24*60*60+1..3*24*60*60 -> "позавчера"
         else -> "давно"

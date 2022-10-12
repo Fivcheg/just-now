556849279072fd4541926f451df13bc25a6eb02f
Vitalii Novgorodov <fivcheg@gmail.com>
2022-10-12 19:46:05 +0300

    Add fun agoToText

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
-fun main(args: Array<String>) {
-    val timeInSeconds: 3600
-    
-    println("Hello World!")
+fun main() {
+    val timeInSeconds = 10
+    val finish = agoToText(timeInSeconds)
+    println("Был(-а) в сети $finish")
+}
 
-
+fun agoToText(timeInSecond: Int): String{
+    val result = when (timeInSecond){
+        in 0..60 -> "только что"
+        in 61..60*60 -> ""
+        in 60*60+1..24*60*60 -> ""
+        in 24*60*60+1..2*24*60*60 -> "вчера"
+        in 2*24*60*60+1..3*24*60*60 -> "позавчера"
+        else -> "давно"
+    }
+    return result;
 }
